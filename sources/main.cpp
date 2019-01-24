70dcf0a74cab68a2614d4373f1e90f8e7b4a87fe
Jb_dumat <jean-baptiste.dumat@epitech.eu>
2019-01-24 16:44:36 +0100

    [ADDED] Setup repo with basic CmakeList, conanfile and sources

---
+++ sources/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** CPP_zia_github
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(int, char *[])
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
