4e05100404c655f44536c01fda02d52dbf46d8c4
Guillaume Guerin <guillaume.guerin@epitech.eu>
2019-01-26 19:20:02 +0100

    [ADD] Google Test in Conan / test demo with add function

---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+#include <math.h>
+ 
+double add(const int a, const int b) {
+    return a + b; 
+}
---
+++ tests/demo.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "src/test.cpp"
+
+TEST(add, addFunction)
+{
+    EXPECT_EQ(25, add(20, 5));
+    EXPECT_EQ(0, add(0, 0));
+    EXPECT_EQ(0, add(1, -1));
+}
