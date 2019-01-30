a4d83ee2707c5843983f3b834cb7f684b76253fd
lwk <louis.waguet-kobayashi@epitech.eu>
2019-01-30 21:50:02 +0100

    [ADDED] Doxygen for EModuleStatus

--- api/EModuleStatus.hpp
+++ api/EModuleStatus.hpp
@@ -7,10 +7,18 @@
 
 #pragma once
 
+/*! \namespace Pizzia
+ *
+ * Namespace used for the Pizzia API
+ */
 namespace Pizzia {
+	/*!
+	 * \enum EModuleStatus
+	 * \brief Constants used for getting/setting the status of a module.
+	 */
 	enum EModuleStatus {
-		SUCCESS,
-		ERROR,
-		FATAL_ERROR,
+		SUCCESS, /*!< Module is successfully running. */
+		ERROR, /*!< Module has encountered an error. */
+		FATAL_ERROR, /*!< Module has encountered a critical error. */
 	};
 }
