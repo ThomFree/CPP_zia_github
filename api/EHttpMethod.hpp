42e7074818a12e9a776c8308358b792e1694e40f
lwk <louis.waguet-kobayashi@epitech.eu>
2019-01-30 21:47:37 +0100

    [ADDED] Doxygen for EHttpMethod

--- api/EHttpMethod.hpp
+++ api/EHttpMethod.hpp
@@ -7,16 +7,24 @@
 
 #pragma once
 
+/*! \namespace Pizzia
+ *
+ * Namespace used for the Pizzia API
+ */
 namespace Pizzia {
+	/*!
+	 * \enum EHttpMethod
+	 * \brief Constants used for HTTP Methods
+	 */
 	enum EHttpMethod {
-		OPTIONS,
-		GET,
-		HEAD,
-		POST,
-		PUT,
-		DELETE,
-		TRACE,
-		CONNECT,
-		UNDEFINED
+		OPTIONS, /*!< Request for information about the communication options available on the request/response chain identified by the Request-URI. */
+		GET, /*!< Retrieve whatever information (in the form of an entity) is identified by the Request-URI. */
+		HEAD, /*!< Identical to GET except that the server MUST NOT return a message-body in the response. */
+		POST, /*!< Used to request that the origin server accept the entity enclosed in the request as a new subordinate of the resource identified by the Request-URI in the Request-Line. */
+		PUT, /*!< Requests that the enclosed entity be stored under the supplied Request-URI. */
+		DELETE, /*!< Requests that the origin server delete the resource identified by the Request-URI. */
+		TRACE, /*!< Used to invoke a remote, application-layer loop- back of the request message. */
+		CONNECT, /*!< For use with a proxy that can dynamically switch to being a tunnel (e.g. SSL tunneling). */
+		UNDEFINED /*!< Undefined method. */
 	};
 }
