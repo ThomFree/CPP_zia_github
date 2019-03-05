c0d8c9eb5615d0cfa98985a42ac55f1357d72de3
Jb_dumat <jean-baptiste.dumat@epitech.eu>
2019-03-05 01:37:57 +0100

    [MOD] Fixed issue with vector of Clients. Changed to vector of unique ptr of Clients

--- src/server/Client.cpp
+++ src/server/Client.cpp
@@ -12,9 +12,8 @@ namespace Zia {
 
 Client::Client(unsigned int id, std::shared_ptr<net::TCPClient> &sock, dems::config::Config &conf) : _id(id), _tcpClient(sock), _conf(conf) // TODO recevoir la config, les modules
 {
-	printMessage("AVANT");
-	_tcpClient->socket()->setReceive([this](const char *data, size_t size) { readMsg(data, size); printMessage("APRES"); });
-	_tcpClient->socket()->send("Hello World", 11);
+	_tcpClient->socket()->setReceive([&](const char *data, size_t size) { readMsg(data, size); });
+	_tcpClient->socket()->send("Hello World\n", 12);
 }
 
 Client::~Client()
@@ -40,7 +39,7 @@ void Client::printMessage(const std::string &str)
 {
 	std::string name(std::get<std::string>(_conf["name"].v));
 
-	std::cout << "\t\t[Client (" << name << ")]" << str << std::endl;
+	std::cout << "\t\t[Client (" << name << ")] -> " << str << std::endl;
 }
 
 }
