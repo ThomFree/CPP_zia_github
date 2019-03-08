634ded53b2b8d51c89a5b53660a1ef07b5f569e9
Guillaume Guerin <guillaume.guerin@epitech.eu>
2019-03-08 19:11:00 +0100

    [ADD] test exemple

--- src/tests/demo.cpp
+++ src/tests/demo.cpp
@@ -2,11 +2,11 @@
 // Created by Guillaume Guerin on 2019-03-08.
 //
 
-#include "gtest/gtest.h"
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
 #include "./testdemo.cpp"
 
-TEST(add, addFunction) {
-	EXPECT_EQ(25, add(20, 5));
-	EXPECT_EQ(0, add(0, 0));
-	EXPECT_EQ(0, add(1, -1));
+BOOST_AUTO_TEST_CASE( test1 )
+{
+	BOOST_TEST( add(20, 0) == 25 );
 }
